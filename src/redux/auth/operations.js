90f655af8067d0bd1b222d164d1661e09f8475d8
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-11 20:33:35 +0300

    add setAuthHeader

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,22 +3,26 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://connections-api.goit.global/";
 
+const setAuthHeader = (value) => {
+  axios.defaults.headers.Authorization = value;
+};
+
 export const register = createAsyncThunk("auth/register", async (values) => {
   const res = await axios.post("/users/signup", values);
 
-  axios.defaults.headers.Authorization = `Bearer ${res.data.token}`; // носій
+  setAuthHeader(`Bearer ${res.data.token}`);
   return res.data;
 });
 
 export const login = createAsyncThunk("auth/login", async (values) => {
   const res = await axios.post("/users/login", values);
 
-  axios.defaults.headers.Authorization = `Bearer ${res.data.token}`; // носій
+  setAuthHeader(`Bearer ${res.data.token}`);
   return res.data;
 });
 
 export const logout = createAsyncThunk("auth/logout", async () => {
   const res = await axios.post("/users/logout");
 
-  axios.defaults.headers.Authorization = "";
+  setAuthHeader("");
 });
